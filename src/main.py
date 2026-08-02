0233013a6561849d1e52bd2092082a17c32b13f5
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-01 22:16:49 -0500

    feat: incluir el enrutador de autenticación en la aplicación FastAPI

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 from src.database import engine, Base
 from contextlib import asynccontextmanager
 from src.domains.resources.router import router as resources_router
+from src.domains.auth.router import router as auth_router
 
 
 app = FastAPI()
 app.include_router(resources_router)
+app.include_router(auth_router)

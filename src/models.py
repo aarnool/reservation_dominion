045d0585bed8c394eddedee493e7735b8b5eb528
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-01 22:16:58 -0500

    feat: agregar modelo de usuario a la importación en models.py

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from src.domains.auth.models import Role
+from src.domains.auth.models import Role, User
 from src.domains.notifications.models import Notification
 from src.domains.reservations.models import Reservation
 from src.domains.resources.models import Resources

d758076a122df1fcc9d60ea54b9d1143f3dda2f5
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-03 00:03:57 -0500

    feat: incluir el enrutador de reservas en la aplicación FastAPI

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from src.database import engine, Base
 from contextlib import asynccontextmanager
 from src.domains.resources.router import router as resources_router
 from src.domains.auth.router import router as auth_router
+from src.domains.reservations.router import router as reservations_router
 
 
 app = FastAPI()
 app.include_router(resources_router)
 app.include_router(auth_router)
+app.include_router(reservations_router)

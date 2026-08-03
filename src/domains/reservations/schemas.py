5be02b97ef9d7139ca2526f5388039826179a1da
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-03 00:04:23 -0500

    feat: agregar esquemas para la creación, respuesta y actualización de reservas en el modelo

---
+++ src/domains/reservations/schemas.py
@@ -0,0 +1,67 @@
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+from src.domains.reservations.models import StatusReservation
+
+# Esquemas para la creación, actualización y respuesta de reservas
+class ReservationBase(BaseModel):
+
+    title: str = Field(
+        description="Nombre del recurso")
+    description: str | None = Field(
+        default=None,
+        description="Descripción del recurso") 
+    start_time: datetime = Field(
+        description="Fecha y hora de inicio de la reserva")
+    end_time: datetime = Field(
+        description="Fecha y hora de finalización de la reserva")
+    
+
+
+# Esquemas para la creación de una reserva inyectable para usar en los endpoints Body
+class ReservationCreate(ReservationBase):
+
+    resource_id: int = Field(
+        description="Identificador del recurso a reservar")
+
+
+# Esquemas para la respuesta de una reserva, incluyendo campos adicionales que deberia tener una reserva en la base de datos
+class ReservationResponse(ReservationBase):
+
+    id: int = Field(
+        description="Identificador único de la reserva")
+    user_id: int = Field(
+        description="Identificador del usuario que realizó la reserva")
+    status_reservation: StatusReservation = Field(
+        default=StatusReservation.PENDING,
+        description="Estado de la reserva")
+    resource_id: int = Field(
+        description="Identificador del recurso reservado")
+    created_at: datetime = Field(
+        description="Fecha y hora de creación de la reserva")
+    updated_at: datetime = Field(
+        description="Fecha y hora de última actualización de la reserva")
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+
+
+# Esquemas para la actualización de una reserva, permitiendo actualizar solo ciertos campos
+class ReservationUpdate(BaseModel):
+
+    title: str | None = Field(
+        default=None,
+        description="Nombre del recurso")
+    description: str | None = Field(
+        default=None,
+        description="Descripción del recurso") 
+    start_time: datetime | None = Field(
+        default=None,
+        description="Fecha y hora de inicio de la reserva")
+    end_time: datetime | None = Field(
+        default=None,
+        description="Fecha y hora de finalización de la reserva")
+    status_reservation: StatusReservation | None = Field(
+        default=None,
+        description="Estado de la reserva")

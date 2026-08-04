245b0c8a9a402e81113653f4708ae7f7ef33bbd2
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-04 13:10:04 -0500

    refactor: mejorar la organización de los permisos en ROLES_SCOOPES añadiendo comentarios descriptivos

--- src/core/permissions.py
+++ src/core/permissions.py
@@ -1,9 +1,11 @@
 ROLES_SCOOPES = {
     "admin": [
+        # Permisos de Recursos 
         "resources:create", 
         "resources:read", 
         "resources:update", 
         "resources:delete", 
+        # Permisos de Reservaciones
         "reservations:create", 
         "reservations:read", 
         "reservations:update", 
@@ -12,9 +14,12 @@ ROLES_SCOOPES = {
     ],
     
     "user": [
+        # Permisos de Recursos
         "resources:read", 
+        # Permisos de Reservaciones
         "reservations:create", 
         "reservations:read", 
         "reservations:update", 
-        "reservations:delete"],
+        "reservations:delete"
+    ]
 }

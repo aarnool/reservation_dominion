adc90ec156103dce2d0ea095b03612c1caa851a0
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-05 01:08:03 -0500

    test: agregar pruebas para la obtención y gestión de recursos por usuarios normales

---
+++ test/user/test_resources_user.py
@@ -0,0 +1,83 @@
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.domains.resources.models import Resources
+
+
+
+# Prueba obtener la lista de recursos como usuario normal
+async def test_user_get_resources_success(user_client: AsyncClient, db_session: AsyncSession):
+
+    resource = Resources(
+        name="Resource User 1", 
+        capacity=10
+    )
+    db_session.add(resource)
+    await db_session.commit()
+    
+    response = await user_client.get("/resources/")
+    assert response.status_code == 200
+
+
+
+# Prueba obtener un recurso por su ID como usuario normal
+async def test_user_get_resource_by_id_success(user_client: AsyncClient, db_session: AsyncSession):
+  
+    resource = Resources(
+        name="Resource User 2", 
+        capacity=20
+    )
+    db_session.add(resource)
+    await db_session.commit()
+    await db_session.refresh(resource)
+    
+    response = await user_client.get(f"/resources/{resource.id}")
+    assert response.status_code == 200
+
+
+
+# Prueba obtener un recurso inexistente y espera un 404
+async def test_user_get_resource_not_found(user_client: AsyncClient):
+    
+    response = await user_client.get("/resources/99999")
+    assert response.status_code == 404
+
+
+
+# Verifica que un usuario no administrador no pueda crear un recurso (403 Forbidden).
+async def test_user_cannot_create_resource(user_client: AsyncClient):
+
+    response = await user_client.post(
+        "/resources/", json={"name": "New Res", "capacity": 30}
+    )
+    assert response.status_code == 403
+
+
+
+# Verifica que un usuario no administrador no pueda actualizar un recurso (403 Forbidden).
+async def test_user_cannot_update_resource(user_client: AsyncClient, db_session: AsyncSession):
+
+    resource = Resources(
+        name="Resource User 3", 
+        capacity=10)
+    db_session.add(resource)
+    await db_session.commit()
+    await db_session.refresh(resource)
+    
+    response = await user_client.patch(f"/resources/{resource.id}", json={"capacity": 50})
+    assert response.status_code == 403
+
+
+
+# Verifica que un usuario no administrador no pueda eliminar un recurso (403 Forbidden).
+async def test_user_cannot_delete_resource(user_client: AsyncClient, db_session: AsyncSession):
+
+    resource = Resources(
+        name="Resource User 4", 
+        capacity=10
+    )
+    db_session.add(resource)
+    await db_session.commit()
+    await db_session.refresh(resource)
+    
+    response = await user_client.delete(f"/resources/{resource.id}")
+    assert response.status_code == 403

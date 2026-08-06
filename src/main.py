3d7871b290f43fbbccc3e21dc706cc6bbf09f594
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-05 19:26:07 -0500

    feat: agregar configuración de CORS para permitir solicitudes desde orígenes específicos

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
-import asyncio
 from fastapi import FastAPI
-from src.database import engine, Base
-from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 from src.domains.resources.router import router as resources_router
 from src.domains.auth.router import router as auth_router
 from src.domains.reservations.router import router as reservations_router
 
 
-app = FastAPI()
+app = FastAPI   ()
 app.include_router(resources_router)
 app.include_router(auth_router)
 app.include_router(reservations_router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["X-Total-Count", "Content-Range"]
+)

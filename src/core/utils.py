5d493d3a11c6f284ef70772f3f6c2a19cd445f1e
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-08 18:57:28 -0500

    feat: agregar función get_r2_client para configurar boto3 con R2 de Cloudflare

--- src/core/utils.py
+++ src/core/utils.py
@@ -2,6 +2,8 @@ from datetime import UTC
 
 from sqlalchemy import DateTime
 from sqlalchemy.types import TypeDecorator
+from src.config import settings
+import boto3
 
 
 class TZDateTime(TypeDecorator):
@@ -23,3 +25,14 @@ class TZDateTime(TypeDecorator):
         if value is not None and value.tzinfo is None:
             return value.replace(tzinfo=UTC)
         return value
+
+
+# Configuracion de boto3 para usar R2 de Cloudflare
+def get_r2_client():
+    return boto3.client(
+        "s3",
+        endpoint_url=settings.R2_ENDPOINT,
+        aws_access_key_id=settings.R2_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.R2_SECRET_ACCESS_KEY.get_secret_value(),
+        region_name="auto",  # R2 no requiere una región específica, pero boto3 necesita un valor
+    )

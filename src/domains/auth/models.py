8ee7e6f35e1b0f0fd4a417a862191fabe1ab9673
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-08 19:01:28 -0500

    feat: agregar columna avatar_url en la tabla de usuarios para almacenar URLs de imágenes

--- src/domains/auth/models.py
+++ src/domains/auth/models.py
@@ -27,6 +27,7 @@ class User(Base):
     # Columnas de la tabla de usuarios
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+    avatar_url: Mapped[str] = mapped_column(String(255), nullable=True)
     email: Mapped[str] = mapped_column(String(128), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     first_name: Mapped[str] = mapped_column(String(64), nullable=False)

a5711d07bdc776acfa7bb79765820975fa51c659
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-09 16:53:37 -0500

    feat: eliminar clase FileTypeValidator y sus dependencias del archivo dependencies.py

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,9 +1,8 @@
 from collections.abc import AsyncGenerator
 
 import jwt
-from fastapi import File, HTTPException, Request, UploadFile, status
+from fastapi import HTTPException, Request, status
 from fastapi.security import SecurityScopes
-import magic
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.security import ALGORITHM, SECRET_KEY
@@ -64,33 +63,3 @@ async def get_current_user(request: Request, security_scopes: SecurityScopes) ->
             )
 
     return payload
-
-
-# Dependecia para obtener el MIME type del archivo subido, si no es un archivo valido lanza una excepcion
-class FileTypeValidator:
-    def __init__(self, allowed_types: list[str]):
-        self.allowed_types = allowed_types
-
-    async def __call__(self, file: UploadFile | None) -> UploadFile | None:
-        if file is None:
-            return None
-
-        # Leer cabecera para chequear el Magic Number
-        header_bytes = await file.read(2048)
-        await file.seek(0)  # Resetear cursor
-
-        # Detectar tipo real
-        detected_mime = magic.from_buffer(header_bytes, mime=True)
-
-        if detected_mime not in self.allowed_types:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Archivo no válido. Tipo detectado: {detected_mime}",
-            )
-
-        return file
-
-
-valiate_image_file = FileTypeValidator(
-    allowed_types=["image/jpeg", "image/png", "image/webp", "image/jpg"]
-)

7af6752f8fbfb93846ff7bde03879e39ca645244
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-12 11:33:42 -0500

    feat: agregar función para crear notificaciones para administradores al crear reservas

--- src/domains/notifications/service.py
+++ src/domains/notifications/service.py
@@ -1,5 +1,7 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.domains.auth.models import User
 from src.domains.notifications.models import Notification
 from src.domains.notifications.schemas import NotificationCreate
 
@@ -28,3 +30,30 @@ async def create_notification(
     await db.commit()
     await db.refresh(new_notification)
     return new_notification
+
+
+async def create_notifications_by_admins(db: AsyncSession, reservation_id: int) -> None:
+    """
+    Crea notificaciones para todos los administradores.
+
+    Args:
+        db: Sesión de la base de datos.
+        reservation_id: ID de la reserva para la cual se crean las notificaciones.
+    Returns:
+        None.
+    """
+    # Obtiene todos los usuarios con el rol de administrador (role_id = 2)
+    result = await db.execute(select(User).where(User.role_id == 2))
+    admins_list = result.scalars().all()
+
+    for admin in admins_list:
+        new_notification = Notification(
+            message=f"Se ha creado una nueva reserva con ID {reservation_id}",
+            type_notification="reservation",
+            is_read=False,
+            user_id=admin.id,
+            reservation_id=reservation_id,
+        )
+        db.add(new_notification)
+        await db.commit()
+        await db.refresh(new_notification)

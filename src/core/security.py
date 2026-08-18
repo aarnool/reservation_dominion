5d16e7b7fbf64f84d1d55f893005452f01356721
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-17 22:01:29 -0500

    refactor: se movio la dependencia de numeros magic a core.segurity

--- src/core/security.py
+++ src/core/security.py
@@ -1,6 +1,8 @@
 from datetime import UTC, datetime, timedelta
 
 import jwt
+import magic
+from fastapi import HTTPException, UploadFile, status
 from pwdlib import PasswordHash
 
 from src.config import settings
@@ -33,3 +35,33 @@ def create_access_token(data: dict, expires_delta: int = ACCESS_TOKEN_EXPIRE_MIN
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+# Dependecia para obtener el MIME type del archivo subido, si no es un archivo valido lanza una excepcion
+class FileTypeValidator:
+    def __init__(self, allowed_types: list[str]):
+        self.allowed_types = allowed_types
+
+    async def __call__(self, file: UploadFile | None = None) -> UploadFile | None:
+        if file is None:
+            return None
+
+        # Leer cabecera para chequear el Magic Number
+        header_bytes = await file.read(2048)
+        await file.seek(0)  # Resetear cursor
+
+        # Detectar tipo real
+        detected_mime = magic.from_buffer(header_bytes, mime=True)
+
+        if detected_mime not in self.allowed_types:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Archivo no válido. Tipo detectado: {detected_mime}",
+            )
+
+        return file
+
+
+valiate_image_file = FileTypeValidator(
+    allowed_types=["image/jpeg", "image/png", "image/webp", "image/jpg"]
+)

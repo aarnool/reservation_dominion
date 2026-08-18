8502c33a9c0924b61d5b1f23883398a25252505b
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-08-17 22:18:53 -0500

    test: agregar prueba para obtener todas las notificaciones con código de estado 200 OK

---
+++ test/notifications/test_notifications_sucess.py
@@ -0,0 +1,21 @@
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.domains.notifications.models import Notification
+
+
+# Prueba para obtener todas las notificaciones y espera un código de estado 200 OK
+async def test_get_all_notifications(
+    admin_client: AsyncClient, db_session: AsyncSession
+):
+    notification = Notification(
+        type_notification="info",
+        message="Este es un mensaje de prueba",
+        user_id=1,
+    )
+    db_session.add(notification)
+    await db_session.commit()
+
+    response = await admin_client.get("/notifications/")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
